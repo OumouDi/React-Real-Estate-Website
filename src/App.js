4c2206955f96d5a9058543b97f504584f7f70a7c
OumouDi <124316387+OumouDi@users.noreply.github.com>
2024-02-29 10:56:41 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Hero from "./components/hero/Hero";
 import Residencies from "./components/residencies/Residencies";
 import Values from "./components/values/Values";
-
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -15,8 +15,7 @@ function App() {
       </div>
       <Residencies />
       <Values />
-
-
+      <Contact />
     </div>
   );
 }
